2aa68a0f60ae868fd5215e74a08cf108a6731ddd
Emon <emonhasan7650@gmail.com>
2023-10-15 23:27:05 +0600

    vercel api added

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-catch */
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
